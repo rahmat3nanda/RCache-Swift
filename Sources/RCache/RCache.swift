4c6f0cd1451718001120478c40a7cf53679b5512
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-18 22:54:54 +0700

    Create RCache.swift & Key

--- Sources/RCache/RCache.swift
+++ Sources/RCache/RCache.swift
@@ -1,2 +1,35 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+//
+//  RCache.swift
+//  RCache
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 18/08/24.
+//
+
+import Foundation
+
+/**
+ This is a class for managing cache
+ */
+public class RCache {
+    
+    private init() {}
+}
+
+// MARK: - Key
+public extension RCache {
+    class Key {
+        public var rawValue: String
+        
+        
+        public init(_ rawValue: String) {
+            self.rawValue = rawValue
+        }
+        
+        public init(rawValue: String) {
+            self.rawValue = rawValue
+        }
+    }
+}
